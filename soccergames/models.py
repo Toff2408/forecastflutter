3618c3f7b0064ed4599dec7d9318feb344b73e81
Toff2408 <joshuaeugene92@yahoo.com>
2023-12-07 08:40:35 -0600

    date query updated

--- soccergames/models.py
+++ soccergames/models.py
@@ -21,9 +21,12 @@ class Country(models.Model):
 class League(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     league = models.CharField(max_length=200)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.league
+
+
 
 
 class Team(models.Model):
@@ -33,26 +36,31 @@ class Team(models.Model):
     def __str__(self):
         return self.name
 
-
 class Headtohead(models.Model):
-    
-    team = models.ManyToManyField(Team)
-    g1 = models.CharField(max_length=1, default='W')
-    g2 = models.CharField(max_length=1, default='W')
-    g3 = models.CharField(max_length=1, default='W')
-    g4 = models.CharField(max_length=1, default='W')
-    g5 = models.CharField(max_length=1, default='W')
+    first_team = models.ForeignKey(Team, on_delete=models.CASCADE,related_name='firstteam',null=True, blank=True)
+    second_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='secondteam',null=True, blank=True)
+    result = models.CharField(max_length=100, null=True,blank=True)
+    date = models.DateField(null=True,blank=True)
 
     def __str__(self):
-        return self.g1
+        return str(self.first_team)
+
+class GameDates(models.Model):
+    # game = models.ForeignKey('Game', on_delete=models.CASCADE)
+    # date = models.DateTimeField(default=datetime.now())
+    # dates = models.DateField(widget=dates(format='%d-%m-%Y'), 
+    #                    input_formats=['%d-%m-%Y'])
+    dates = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.dates) 
 
 
 class Game(models.Model):
-    headhead = models.ForeignKey(Headtohead, on_delete=models.CASCADE, null=True)
+    gamedates = models.ForeignKey(GameDates, on_delete=models.CASCADE, null=True, blank=True)
+    headhead = models.ForeignKey(Headtohead, on_delete=models.CASCADE,null=True,blank=True)
     home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_team', null=True, blank=True)
     away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_team', null=True, blank=True)
-    # g_country = models.ForeignKey(Country,on_delete=models.CASCADE,null=True, blank=True)
-    # g_league = models.ForeignKey(League,on_delete=models.CASCADE,related_name='leagues')
     date = models.TimeField()
     home_odd = models.FloatField()
     draw_odd = models.FloatField()
@@ -60,14 +68,16 @@ class Game(models.Model):
     to_win = models.IntegerField(default=1)
     goals = models.CharField(max_length=1)
     result = models.BooleanField(null=True, blank=True)
-<<<<<<< HEAD
     lastfive_home = models.CharField(max_length=5, default='LLLLL')
     lastfive_away = models.CharField(max_length=5, default='WWWWW')
+    won = models.CharField(max_length=100, null=True,blank=True)
+    notwon = models.CharField(max_length=100,null=True,blank=True)
+    overone = models.BooleanField(null=True, blank=True)
+    overtwo = models.BooleanField(null=True, blank=True)
+    overthree = models.BooleanField(null=True, blank=True)
 
-=======
-    last_fivehome = models.CharField(max_length=5, default='LLLLL')
-    last_fiveaway = models.CharField(max_length=5, default='LLLLL')
->>>>>>> master
 
     def __str__(self):
         return str(self.home_team) + ' - ' + str(self.away_team)
+
+    
--- soccergames/views.py
+++ soccergames/views.py
@@ -1,16 +1,54 @@
 from django.shortcuts import render
 from .models import *
+from datetime import date
 
 
 
 def index(request):
-    games = Game.objects.all().order_by('home_team__league')
+    format = date.today()
+    games = Game.objects.all().order_by('home_team__league').filter(gamedates__dates=format)
+    premier = League.objects.all().order_by('league')
     
     context = {
-        "games": games
+        "games": games,
+        "premier":premier
     }
 
     return render(request, 'index.html', context)
 
-def detail(request):
-    return render(request, 'detail.html')
+def detail(request, id, slug):
+    details = Game.objects.get(pk = id)
+    head = Headtohead.objects.get(pk=1)
+   
+    context = {
+        "details":details,
+        "head":head
+    }
+                    
+    return render(request, 'detail.html', context)
+
+# def indexx(request):
+#     premier = Game.objects.filter(premier="premier")
+    
+#     context = {
+#         "premier": premier,
+#     }
+#     return render(request, "index.html", context)
+
+
+# def indexx(request):
+#     premier = Game.objects.filter(headhead__first_team__league='league')
+    
+#     context = {
+#         "premier": premier,
+#     }
+#     return render(request, "index.html", context)
+
+# def indexx(request):
+#     premier = Product.objects.filter(category__title= title)
+#     premier = Date.objects.filter(category__title= title)
+    
+#     context = {
+#         "premier": premier,
+#     }
+#     return render(request, "index.html", context)
